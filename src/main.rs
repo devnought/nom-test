b250719167a4b783bbabe041a0bc8f2da852463c
Kyle Gretchev <kgretchev@gmail.com>
2019-06-07 03:18:16 -0400

    Simplified line end parsing

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,12 @@ fn maybe_whitespace(i: &str) -> IResult<&str, &str> {
 fn line_end(i: &str) -> IResult<&str, &str> {
     let crlf = tag("\r\n");
     let lf = tag("\n");
+    let ending = alt((crlf, lf));
+    let parser = tuple((maybe_whitespace, ending));
 
-    alt((crlf, lf))(i)
+    let (input, (_, end)) = parser(i)?;
+
+    Ok((input, end))
 }
 
 fn string(i: &str) -> IResult<&str, &str> {
@@ -53,16 +57,16 @@ fn string(i: &str) -> IResult<&str, &str> {
 
 fn host_line(i: &str) -> IResult<&str, &str> {
     let host = tag("Host");
-    let parser = tuple((maybe_whitespace, host, whitespace, string, maybe_whitespace, line_end));
+    let parser = tuple((maybe_whitespace, host, whitespace, string, line_end));
 
-    let (input, (_, _, _, name, _, _)) = parser(i)?;
+    let (input, (_, _, _, name, _)) = parser(i)?;
 
     Ok((input, name))
 }
 
 fn property_line(i: &str) -> IResult<&str, (&str, &str)> {
-    let parser = tuple((maybe_whitespace, string, whitespace, string, maybe_whitespace, line_end));
-    let (input, (_, key, _, value, _, _)) = parser(i)?;
+    let parser = tuple((maybe_whitespace, string, whitespace, string, line_end));
+    let (input, (_, key, _, value, _)) = parser(i)?;
 
     Ok((input, (key, value)))
 }
