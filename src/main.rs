d17d58d79efeccf19e3acf3e5722454868adb6d8
Kyle Gretchev <kyle.gretchev@magnetforensics.com>
2019-06-13 14:07:15 -0400

    Collections of properties seem to parse ok now

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@
 // https://linux.die.net/man/5/ssh_config
 
 use nom::{
-    branch::alt,
     bytes::complete::{tag, take_while1},
-    character::complete::{line_ending, not_line_ending, space0, space1},
-    combinator::{opt, peek},
-    multi::{many0, many_till},
+    character::complete::{line_ending, multispace0, not_line_ending, space0, space1},
+    combinator::{map, not, opt, peek},
+    multi::many0,
     sequence::tuple,
     IResult,
 };
@@ -27,10 +26,10 @@ fn main() {
         std::process::exit(1);
     });
 
-    println!("{:#?}", hosts(&data));
+    println!("{:#?}", host_line(&data));
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 struct Host<'a> {
     name: &'a str,
     properties: Vec<Property<'a>>,
@@ -61,7 +60,10 @@ fn host_line(i: &str) -> IResult<&str, &str> {
 // Property { key, values }
 // to handle the case where a key has multple values, like for `LocalForward`
 fn property_line(i: &str) -> IResult<&str, Property> {
+    not(peek(host_line))(i)?;
+
     let parser = tuple((space0, string, space1, not_line_ending, opt(line_ending)));
+
     let (input, (_, key, _, value, _)) = parser(i)?;
 
     Ok((
@@ -73,26 +75,14 @@ fn property_line(i: &str) -> IResult<&str, Property> {
     ))
 }
 
-fn properties(i: &str) -> IResult<&str, (Vec<Property>, &str)> {
-    let parser = alt((peek(host_line), line_ending));
-    many_till(property_line, parser)(i)
-}
+fn properties(i: &str) -> IResult<&str, Vec<Property>> {
+    let parser = many0(tuple((multispace0, property_line, multispace0)));
 
-fn host_block(i: &str) -> IResult<&str, Host> {
-    let parser = tuple((host_line, properties));
-    let (input, (host, p)) = parser(i)?;
-    let (props, _) = p;
+    let (input, props) = map(parser, |props| {
+        props.into_iter().map(|(_, p, _)| p).collect()
+    })(i)?;
 
-    let host_struct = Host {
-        name: host,
-        properties: props,
-    };
-
-    Ok((input, host_struct))
-}
-
-fn hosts(i: &str) -> IResult<&str, Vec<Host>> {
-    many0(host_block)(i)
+    Ok((input, props))
 }
 
 #[cfg(test)]
@@ -100,12 +90,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn host_line_newline() {
+    fn host_line_newline_linefeed() {
         let (input, host) =
             host_line("Host dev\n").expect("Could not parse host line ending in '\\n'");
         assert_eq!("", input);
         assert_eq!("dev", host);
+    }
 
+    #[test]
+    fn host_line_newline_carriagereturn_linefeed() {
         let (input, host) =
             host_line("Host dev-man\r\n").expect("Could not parse host line ending in '\\r\\n'");
         assert_eq!("", input);
@@ -115,7 +108,7 @@ mod tests {
     #[test]
     fn host_line_no_newline() {
         let (input, host) =
-            host_line("Host dev").expect("Could not parse host line without terminating newline");
+            host_line("Host dev").expect("Could not parse host line with no newline");
         assert_eq!("", input);
         assert_eq!("dev", host);
     }
@@ -123,48 +116,33 @@ mod tests {
     #[test]
     fn property_line_newline() {
         let (input, property) =
-            property_line("Asd 123\n").expect("Could not parse property line ending in '\\n'");
-        assert_eq!("", input);
-        assert_eq!(
-            Property {
-                key: "Asd",
-                value: "123"
-            },
-            property
-        );
+            property_line("      LocalForward      9906 127.0.0.1:3306        \n")
+                .expect("Could not parse property line with line ending in '\\n'");
 
-        let (input, property) = property_line("Qwe hello there   \r\n")
-            .expect("Could not parse property line ending in '\\r\\n'");
+        let expected_property = Property {
+            key: "LocalForward",
+            value: "9906 127.0.0.1:3306",
+        };
+
         assert_eq!("", input);
-        assert_eq!(
-            Property {
-                key: "Qwe",
-                value: "hello there"
-            },
-            property
-        );
+        assert_eq!(expected_property, property);
     }
 
     #[test]
-    fn property_line_no_newline() {
-        let (input, property) = property_line("Test man")
-            .expect("Could not parse property line without terminating newline");
-        assert_eq!("", input);
-        assert_eq!(
-            Property {
-                key: "Test",
-                value: "man"
-            },
-            property
-        );
-    }
+    fn multiple_properties() {
+        let (input, properties) = properties(
+            "   \n\n\n HostName database.example.com\n    IdentityFile ~/.ssh/coolio.example.key\n\n\n\n\n\n\nAsd 123",
+        )
+        .expect("Coult not parse property collection");
 
-    #[test]
-    fn properties_end_newline() {
-        let _expected_properties = vec![
+        let expected_properties = vec![
             Property {
-                key: "Test",
-                value: "man",
+                key: "HostName",
+                value: "database.example.com",
+            },
+            Property {
+                key: "IdentityFile",
+                value: "~/.ssh/coolio.example.key",
             },
             Property {
                 key: "Asd",
@@ -172,8 +150,37 @@ mod tests {
             },
         ];
 
-        let (input, _properties) =
-            properties("Test man\nAsd 123\n").expect("Could not parse properties");
         assert_eq!("", input);
+        assert_eq!(expected_properties, properties);
+    }
+
+    #[test]
+    fn multiple_properties_hostline_end() {
+        let (input, properties) = properties(
+            "   \n\n\n HostName database.example.com\n     \
+             IdentityFile ~/.ssh/coolio.example.key\
+             \n\n\n\n\n\n\n\
+             Asd 123\n      \
+             Host devv\n\n",
+        )
+        .expect("Coult not parse property collection");
+
+        let expected_properties = vec![
+            Property {
+                key: "HostName",
+                value: "database.example.com",
+            },
+            Property {
+                key: "IdentityFile",
+                value: "~/.ssh/coolio.example.key",
+            },
+            Property {
+                key: "Asd",
+                value: "123",
+            },
+        ];
+
+        assert_eq!("Host devv\n\n", input);
+        assert_eq!(expected_properties, properties);
     }
 }
